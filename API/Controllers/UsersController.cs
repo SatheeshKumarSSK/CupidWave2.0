6aaf1278354e3e6c7c9a048a422c5726321f6161
Satheesh Kumar <s.satheeshkumar2310@gmail.com>
2024-08-12 00:21:43 +0530

    Walking Skeleton - API

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using API.Data;
+using API.Entities;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController(DataContext context) : ControllerBase
+    {
+        private readonly DataContext _context = context;
+
+        // GET: api/Users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetAppUsers()
+        {
+            return await _context.AppUsers.ToListAsync();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetAppUser(int id)
+        {
+            var appUser = await _context.AppUsers.FindAsync(id);
+
+            if (appUser == null) return NotFound();
+
+            return appUser;
+        }
+    }
+}
